e75ca880e303535697bdf3fd4b87f73aee5d0c15
Jose07082001 <josenivelamejia8@gmail.com>
2024-07-03 22:47:03 -0500

    Add files via upload

---
+++ hotels/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS hotel;
+CREATE TABLE hotel (
+    hotel_id int NOT NULL AUTO_ INCREMENT, 
+    hotel_name varchar (100) NOT NULL, 
+    hotel address varchar (100) NOT NULL, 
+    PRIMARY KEY (hotel id)
+);
+
+INSERT INTO hotel (hotel_name, hotel address)
+VALUES ('HOTEL REY DORMILON', 'AVENIDA LAS AGUAS');
+INSERT INTO hotel (hotel name, hotel _address)
+VALUES ('HOTEL ESTANCIA', 'av. quito y pio montufar');
